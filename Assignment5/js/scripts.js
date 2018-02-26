c4868048a10f8f0d4246e18491ea74cfb528ff17
Unknown <heint1@gmatc.matc.edu>
2018-02-26 16:12:14 -0600

    script constructor

---
+++ Assignment5/js/scripts.js
@@ -0,0 +1,13 @@
+var packages = [
+  {name: " ", desc: " ", author: " ", downloads: 20, stars: 35, image: "url"},
+
+]
+
+function packages (name, desc, author, downloads, starts, image) {
+  this.name = name;
+  this.desc = desc;
+  this.author = author;
+  this.downloads = downloads;
+  this.starts = starts;
+  this.image = image;
+}
