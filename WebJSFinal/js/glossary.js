0a1155a454cbdaa8e3f2d52ba54da248f4585ab4
Unknown <heint1@gmatc.matc.edu>
2018-04-30 13:06:02 -0500

    Added terms to glossary js

--- WebJSFinal/js/glossary.js
+++ WebJSFinal/js/glossary.js
@@ -7,8 +7,200 @@ var terms = [
 
   {
     category: 'Products',
-    word: 'Domestic',
-    definition: 'This is an example of a definition that is here to hold space.'
+    word: 'Domestic Crops',
+    definition: 'Domestic crops are purchase and eaten right in your own country. While there is demand outside of your country as well, most of these products are consumed at home. This can change, for instance, at the whim of fad diets in wealthier countries. Domestic unrest can wreak havoc on their production and sale. Since they are fairly cheap to grow and ubiquitous (all over the place), they are cheap and relatively safe to plant but don’t usually yield a big profit. New technology could make these products more lucrative in time.'
+  },
+
+  {
+    category: 'Products',
+    word: 'Cash Crops',
+    definition: 'The demand of cash crops is generally driven by wealthier foreign markets. In this case that includes Europe, China, Russia, and the US. This does however, make them much more susceptible (vulnerable or exposed) to abrupt changes in the market based on the countries perpetually (ongoing with no sign of stopping) political turmoil. They are risky and expensive to plant but they can also be big earners. Private corporations play a big role in this market with fortunes to be made and lost in the blink of an eye.'
+  },
+
+  {
+    category: 'Products',
+    word: 'Livestock',
+    definition: 'Livestock are an all-around safe bet when it comes to Caribbean farming. The only problem is that during periods of conflict they make an easy meal for any armed group passing through whether they be the army or a rebel faction. They are a fairly expensive investment but offer a relatively stable income with solid returns.'
+  },
+
+  {
+    category: 'Products',
+    word: 'Illegal Crops',
+    definition: 'Illegal Crops are exactly what they sound like. Illegal. This, however, means that corrupt officials and politicians may be willing to look the other way if a persuasive argument can be made if you catch my drift. The price of these illicit goods depends heavily on the government’s attitude towards them. This doesn’t always mean that a crackdown hurts the production end though. They do tend to be more heavily taxed than other crops by the banker. Someone has to launder the extra cash.'
+  },
+
+  {
+    category: 'Domestic',
+    word: 'Immigration',
+    definition: 'You country has built the reputation of being safe and prosperous and people from around the region are coming in droves in hopes of better lives. All these new people mean more mouths to feed.'
+  },
+
+  {
+    category: 'Domestic',
+    word: 'Emigration',
+    definition: 'Maybe it’s the lack of healthcare and infrastructure, non-existent job prospects, or the paramilitary death squads but people seem to be leaving the country in droves. Even in small towns, their absence is noticeable and having a negative effect on the economy in general.'
+  },
+
+  {
+    category: 'Domestic',
+    word: 'Raise Minimum Wage',
+    definition: 'A rising tide raises all boats. Many people that were struggling just to get by now have a bit more disposable income for the finer things in life as well as the staples.'
+  },
+
+  {
+    category: 'Domestic',
+    word: 'Urbanization',
+    definition: 'Everybody wants to be where the action is at, the big city. As a result, there are less people living the rural life producing the food. This means less food in general so, anyone still growing the essentials is going to be sought after.'
+  },
+
+  {
+    category: 'Domestic',
+    word: 'Population Boom',
+    definition: 'Prosperity and a general feeling of optimism are leading people to think more about starting a family. Big families are becoming the norm. This means a lot more mouths to feed.'
+  },
+
+  {
+    category: 'Domestic',
+    word: 'Land Redistribution',
+    definition: 'For too long, the vast majority of land in your country has been owned and farmed by foreigners. The new, democratically elected leader of your country has promised to make this right by buying it back, at a fair price, and giving it back to people like yourself.'
+  },
+
+  {
+    category: 'Domestic',
+    word: 'Industrialization',
+    definition: 'New factories have created new ways to produce more crops more efficiently and to sell old products especially more expensive crops.'
+  },
+
+  {
+    category: 'Domestic',
+    word: 'Prohibition',
+    definition: 'Prohibition really limits the supply of illicit products. This means that those who do take the risk to supply them are handsomely rewarded. The downside is that without many in this line of work, your chances of getting caught are fairly high. This has also led to an increase of homemade brews with fermented papayas. Because of its illegality and the risks involved, the banker assesses an extra 20% or 1/5 tax on people participating in the illicit trade.'
+  },
+
+  {
+    category: 'Domestic',
+    word: 'Drug War',
+    definition: 'The war on drugs brings as much risk as it does opportunity. Because it is now arguably more risky to produce drugs, not as many people are involved in the business. Less product means more demand which could mean more profits but, then again, maybe the reason there aren’t as many growers is that people get caught.'
+  },
+
+  {
+    category: 'Domestic',
+    word: 'New Trend',
+    definition: 'Fad diets and celebrity endorsements can have a big impact on demand making obscure fruits and cheeses items worth buying at high end supermarkets.'
+  },
+
+  {
+    category: 'Domestic',
+    word: 'Civil War',
+    definition: 'War may be good for some businesses but agriculture is not one of them. Nothing is being exported due to the bombed out, rebel occupied airfield. What is grown for sale at home is ransacked either by rebels or the army. It’s especially hard on livestock which make a quick meal for any troops passing through. The chaos does do good things for the drug trade with no one to monitor anything as trivial as drugs while fighting continues.'
+  },
+
+  {
+    category: 'Domestic',
+    word: 'Political Unrest',
+    definition: 'Latin America isn’t known as America’s backyard for nothing. Foreign interventions in Latin 	American politics causes a great deal of unrest with different countries and companies trying to buy elections. This leaves farmers who depend on exports to make a living without consistent trading partners depending on who is in charge at that particular moment.'
+  },
+
+  {
+    category: 'Domestic',
+    word: 'Protests',
+    definition: 'Protests are fairly common when the government doesn’t represent the best interests of the people that supposedly elected them. Most of these protesters are the poorer members of society such as truck drivers, 	dock workers, students and even farmers. This makes it difficult to ship anything in or out of the country. Domestic business is also hurt by the fact that people that don’t go to work don’t get paid and can’t buy things.'
+  },
+
+  {
+    category: 'Domestic',
+    word: 'Strikes',
+    definition: 'Sometimes people don’t feel like going to work for a buck a day so they quit. But, every once in a while, they all decide their fed up with it and go on strike. This not only leads to them not having any money to buy your goods but could also affect your country’s ability to export goods depending on who is striking.'
+  },
+
+  {
+    category: 'Domestic',
+    word: 'Military Coup',
+    definition: 'Manuel Noriega, Fidel Castro, Hugo Chavez, Simon Bolivar… the list goes on of Latin American 	strong 	men who came to power by way of the gun. Military coups are not uncommon in many Latin American countries where many factions with many different interests possess plenty of firearms. Government corruption and injustice on a government’s part has a tendency for opening it up to a military takeover in the name of the people. This usually goes hand-in-hand with a healthy dose of law and order. The only question is, when the military overthrows the government, who will be in charge?'
+  },
+
+  {
+    category: 'Domestic',
+    word: 'Genocide',
+    definition: 'When a group of people is being systematically eliminated in your country, business suffers. With genocide and the organized rape and arson of much of your country, your farm lies in ruins. Livestock are eaten and sacrificed and fields are burned to deprive enemies. No matter how you look at it, Genocide is bad for business.'
+  },
+
+  {
+    category: 'Domestic',
+    word: 'High Taxes',
+    definition: 'For whatever reason, the government wanted more of what you work so hard for to do who knows 	what with. The taxes really hit luxury industries hard and the banker is sure to take a higher cut than usual on everything grown.'
+  },
+
+  {
+    category: 'Domestic',
+    word: 'High Import Tariffs',
+    definition: 'The government of your country wants small, independent businesses like yours to grow. One of the ways that they are encouraging this is by charging a pretty hefty tax on crops and luxury items imported to your country. '
+  },
+
+  {
+    category: 'Domestic',
+    word: 'Low Import Tariffs',
+    definition: 'Sometimes, in order to get financial support, small Latin American countries such as yours have 	to give in to other countries’ demands. One of the most common demands they make is that tariffs be lifted to allow free trade which sounds great. However, this also means that huge companies with factory farms can out produce you and undersell you for pennies on the dollar. '
+  },
+
+  {
+    category: 'Domestic',
+    word: 'Inflation',
+    definition: 'The government is broke and they decided the only way out was to print more money. The only problem is that with more of it, it becomes worth less. This has a good side and a bad. It benefits countries that import your goods because they cost them less. So, you may sell more luxury goods but the return on them will be 	less.'
+  },
+
+  {
+    category: 'Domestic',
+    word: 'Tax Cuts',
+    definition: 'Every once in a while the government decides that people can keep more of their money. This could be due to a surplus on their end or a need to stimulate the economy. Either way, the banker is going to collect less taxes and hopefully once people have more money on hand they will spend it on your goods, maybe even something they don’t need.'
+  },
+
+  {
+    category: 'Domestic',
+    word: 'Corrupt Government',
+    definition: 'A corrupt government steals from its people to enhance its own wealth. This theft usually comes in the form of much higher taxes and more control over less reputable sources of income.'
+  },
+
+  {
+    category: 'Domestic',
+    word: 'Monopoly',
+    definition: 'A monopoly involves one company owning the entire supply or manufacturing process of a product. When this happens, they can name their price. Unfortunately, your meager farm is not the one with the monopoly and your prices can’t hope to compete with an international company that has more hard cash than the government of your whole country. '
+  },
+
+  {
+    category: 'Domestic',
+    word: 'Government Price Lock',
+    definition: 'Your leaders made promises during their political campaigns, one of the most popular was 	enough food for every family. This all sounds great but the government doesn’t want to be the one to help pay for it so they set a maximum price you can sell your domestic goods at which has left you without a profit.'
+  },
+
+  {
+    category: 'Domestic',
+    word: 'Legalization of Drugs',
+    definition: 'After many long, bloody years of fighting an unsuccessful war on drugs in your country, the 	government has decided to take a different approach. They argue that by making drugs like coca and marijuana legal, they will reduce the criminal organizations that profit from prohibition. The only problem is now everyone is growing it.'
+  },
+
+  {
+    category: 'Domestic',
+    word: 'Depression',
+    definition: 'The economy took a nosedive, no one saw it coming. Now no one has any cash to buy anything but people will still splurge on a way to forget their problems. The government is asking you to grow food at a loss just to make sure people don’t starve.'
+  },
+
+  {
+    category: 'Domestic',
+    word: 'Investment in Infrastructure',
+    definition: 'New transportation and cheaper utilities options are good for business. It’s cheaper for you to produce and distribute your goods.'
+  },
+
+  {
+    category: 'Domestic',
+    word: 'Weak Currency',
+    definition: 'Your money doesn’t go to far abroad but that could be a good thing. Tourists coming to your country marvel at how cheap everything is relative to their home countries and they are more likely to indulge. This also means that it doesn’t make much sense to buy things that are imported because they almost certainly cost more than domestic products.'
+  },
+
+  {
+    category: 'Domestic',
+    word: 'Strong Currency',
+    definition: 'Strong currency makes importing goods from other countries much more tenable and many people have started to favor foreign products over domestic ones as a result. It’s also harder to sell goods abroad because they cost more in foreign markets.'
   },
 
   {
@@ -92,25 +284,16 @@ function writeTerm(newTerm) {
   } else if (newTerm.category == "Products")
   {
     document.getElementById("products").appendChild(containerEl);
-  } else if (newTerm.category == "Taxes")
+  } else if (newTerm.category == "Domestic")
   {
-    document.getElementById("taxes").appendChild(containerEl);
-  } else if (newTerm.category == "Regulation")
-  {
-    document.getElementById("regulation").appendChild(containerEl);
-  } else if (newTerm.category == "Stability")
-  {
-    document.getElementById("stability").appendChild(containerEl);
+    document.getElementById("domestic").appendChild(containerEl);
   } else if (newTerm.category == "Foreign")
   {
     document.getElementById("foreign").appendChild(containerEl);
-  } else if (newTerm.category == "Labor")
+  } else if (newTerm.category == "Weather")
   {
-    document.getElementById("labor").appendChild(containerEl);
-  } else if (newTerm.category == "Infrastructure")
-  {
-    document.getElementById("infrastructure").appendChild(containerEl);
-  }
+    document.getElementById("weather").appendChild(containerEl);
+  } 
 }
 
 for (var i = 0; i < terms.length; i++){
--- WebJSFinal/js/standards.js
+++ WebJSFinal/js/standards.js
@@ -1,20 +1,134 @@
 var standards = [
   {
     subject: 'Politics',
-    standard: 'A.12.1',
-    rationale: 'This is an example of rationale space holder.'
+    standard: 'C.12.5',
+    rationale: 'Analyze different theories of how governmental powers might be used to help promote or hinder liberty, equality, and justice, and develop a reasoned conclusion'
+  },
+
+  {
+    subject: 'Politics',
+    standard: 'C.12.12',
+    rationale: 'Explain the United States relationship to other nations and its role in international organizations, such as the United Nations, North Atlantic Treaty Organization, World Bank, International Monetary Fund, and North American Free Trade Agreement'
+  },
+
+  {
+    subject: 'Politics',
+    standard: 'C.12.13',
+    rationale: 'Describe and evaluate ideas of how society should be organized and political power should be exercised, including the ideas of monarchism, anarchism, socialism, fascism, and communism; compare these ideas to those of representative democracy; and assess how such ideas have worked in practice'
   },
 
   {
     subject: 'Economics',
-    standard: 'E.12.3',
-    rationale: 'This is another place holder.'
+    standard: 'D.12.1',
+    rationale: 'Explain how decisions about spending and production made by households, businesses, and governments determine the nations levels of income, employment, and prices'
+  },
+
+  {
+    subject: 'Economics',
+    standard: 'D.12.2',
+    rationale: 'Use basic economic concepts (such as supply and demand; production, distribution, and consumption; labor, wages, and capital; inflation and deflation; market economy and command economy) to compare and contrast local, regional, and national economies across time and at the present time'
+  },
+
+  {
+    subject: 'Economics',
+    standard: 'D.12.4',
+    rationale: 'Explain and evaluate the effects of new technology, global economic interdependence, and competition on the development of national policies and on the lives of individuals and families in the United States and the world'
+  },
+
+  {
+    subject: 'Economics',
+    standard: 'D.12.6',
+    rationale: 'Use economic concepts to analyze historical and contemporary questions about economic development in the United States and the world'
+  },
+
+  {
+    subject: 'Economics',
+    standard: 'D.12.7',
+    rationale: 'Compare, contrast, and evaluate different types of economies (traditional, command, market, and mixed) and analyze how they have been affected in the past by specific social and political systems and important historical events'
+  },
+
+  {
+    subject: 'Economics',
+    standard: 'D.12.8',
+    rationale: 'Explain the basic characteristics of international trade, including absolute and comparative advantage, barriers to trade, exchange rates, and balance of trade'
+  },
+
+  {
+    subject: 'Economics',
+    standard: 'D.12.10',
+    rationale: 'Analyze the ways in which supply and demand, competition, prices, incentives, and profits influence what is produced and distributed in a competitive market system'
+  },
+
+  {
+    subject: 'Economics',
+    standard: 'D.12.12',
+    rationale: 'Compare and contrast how values and beliefs, such as economic freedom, economic efficiency, equity, full employment, price stability, security, and growth, influence decisions in different economic systems'
+  },
+
+  {
+    subject: 'Economics',
+    standard: 'D.12.13',
+    rationale: 'Describe and explain global economic interdependence and competition, using examples to illustrate their influence on national and international policies'
   },
 
   {
     subject: 'History',
-    standard: 'H.12.2',
-    rationale: 'This is another place holder.'
+    standard: 'B.12.2',
+    rationale: 'Analyze primary and secondary sources related to a historical question to evaluate their relevance, make comparisons, integrate new information with prior knowledge, and come to a reasoned conclusion'
+  },
+
+  {
+    subject: 'History',
+    standard: 'B.12.1',
+    rationale: 'Explain different points of view on the same historical event, using data gathered from various sources, such as letters, journals, diaries, newspapers, government documents, and speeches'
+  },
+
+  {
+    subject: 'History',
+    standard: 'B.12.3',
+    rationale: 'Recall, select, and analyze significant historical periods and the relationships among them'
+  },
+
+  {
+    subject: 'History',
+    standard: 'B.12.5',
+    rationale: 'Gather various types of historical evidence, including visual and quantitative data, to analyze issues of freedom and equality, liberty and order, region and nation, individual and community, law and conscience, diversity and civic duty; form a reasoned conclusion in the light of other possible conclusions; and develop a coherent argument in the light of other possible arguments'
+  },
+
+  {
+    subject: 'History',
+    standard: 'B.12.8',
+    rationale: 'Recall, select, and explain the significance of important people, their work, and their ideas in the areas of political and intellectual leadership, inventions, discoveries, and the arts, within each major era of Wisconsin, United States, and world history'
+  },
+
+  {
+    subject: 'History',
+    standard: 'B.12.9',
+    rationale: 'Select significant changes caused by technology, industrialization, urbanization, and population growth, and analyze the effects of these changes in the United States and the world'
+  },
+
+  {
+    subject: 'History',
+    standard: 'B.12.13',
+    rationale: 'Analyze examples of ongoing change within and across cultures, such as the development of ancient civilizations; the rise of nation-states; and social, economic, and political revolutions'
+  },
+
+  {
+    subject: 'History',
+    standard: 'B.12.16',
+    rationale: 'Describe the purpose and effects of treaties, alliances, and international organizations that characterize todays interconnected world'
+  },
+
+  {
+    subject: 'Geography',
+    standard: 'A.12.5',
+    rationale: 'Use a variety of geographic information and resources to analyze and illustrate the ways in which the unequal global distribution of natural resources influences trade and shapes economic patterns'
+  },
+
+  {
+    subject: 'Geography',
+    standard: 'A.12.7',
+    rationale: 'Collect relevant data to analyze the distribution of products among global markets and the movement of people among regions of the world'
   }
 ];
 
@@ -25,53 +139,49 @@ function Standard(standards) {
 }
 
 function writeStandard(newStandard) {
-  var subjectTextEl = document.createTextNode(newStandard.subject);
-  var subjectH2El = document.createElement("h2");
   var standardTextEl = document.createTextNode(newStandard.standard);
-  var standardH3El = document.createElement("h3");
+  var standardH2El = document.createElement("h2");
+  var standardColEl = document.createElement("div");
   var rationaleTextEl = document.createTextNode(newStandard.rationale);
   var rationalePEl = document.createElement("p");
+  var rationaleColEl = document.createElement("div");
   var boxEl = document.createElement("div");
-  var col3El = document.createElement("div");
-  var col6El = document.createElement("div");
-  var col9El = document.createElement("div");
-  var rowEl = document.createElement("div");
-  var subjectRowEl = document.createElement("div");
   var standardRowEl = document.createElement("div");
+  var rationaleRowEl = document.createElement("div");
+  var boxColEl = document.createElement("div");
   var containerEl = document.createElement("div");
 
   containerEl.setAttribute("class", "container");
   boxEl.setAttribute("class", "box");
-  rowEl.setAttribute("class", "row");
-  subjectRowEl.setAttribute("class", "row");
   standardRowEl.setAttribute("class", "row");
-  col3El.setAttribute("class", "col-3 col-");
-  col6El.setAttribute("class", "col-6 col-");
-  col9El.setAttribute("class", "col-9 col-");
+  rationaleRowEl.setAttribute("class", "row");
+  boxColEl.setAttribute("class", "col-6 col-");
+  standardColEl.setAttribute("class", "col-");
+  rationaleColEl.setAttribute("class", "col-");
 
-  subjectH2El.appendChild(subjectTextEl);
-  subjectRowEl.appendChild(subjectH2El);
-  standardH3El.appendChild(standardTextEl);
-  standardRowEl.appendChild(standardH3El);
+  standardH2El.appendChild(standardTextEl);
   rationalePEl.appendChild(rationaleTextEl);
-  col9El.appendChild(rationalePEl);
-  col3El.appendChild(subjectRowEl);
-  col3El.appendChild(standardRowEl);
-  rowEl.appendChild(col3El);
-  rowEl.appendChild(col9El);
-  containerEl.appendChild(rowEl);
-  boxEl.appendChild(containerEl);
-  col6El.appendChild(boxEl);
+  standardColEl.appendChild(standardH2El);
+  standardRowEl.appendChild(standardColEl);
+  rationaleColEl.appendChild(rationalePEl);
+  rationaleRowEl.appendChild(rationaleColEl);
+  boxEl.appendChild(standardRowEl);
+  boxEl.appendChild(rationaleRowEl);
+  boxColEl.appendChild(boxEl);
+  containerEl.appendChild(boxColEl);
 
   if (newStandard.subject == "Politics")
   {
-    document.getElementById("politics").appendChild(col6El);
+    document.getElementById("politics").appendChild(containerEl);
   } else if (newStandard.subject == "Economics")
   {
-    document.getElementById("economics").appendChild(col6El);
+    document.getElementById("economics").appendChild(containerEl);
   } else if (newStandard.subject == "History")
   {
-    document.getElementById("history").appendChild(col6El);
+    document.getElementById("history").appendChild(containerEl);
+  } else if (newStandard.subject == "Geography")
+  {
+    document.getElementById("geography").appendChild(containerEl);
   }
 }
 
