c5e882f9b42e53682c4b292c39ccaebb3c8256a4
Unknown <heint1@gmatc.matc.edu>
2018-04-30 08:47:30 -0500

    Auto stash before merge of "master" and "origin/master"

--- WebJSFinal/js/glossaryTabs.js
+++ WebJSFinal/js/glossaryTabs.js
@@ -2,38 +2,6 @@ var getEl = function (id) {
   return document.getElementById(id);
 }
 
-function overviewDisplay() {
-  var headerEl = getEl("main-nav-header");
-  var overview = getEl("overview-div");
-  var argentina = getEl("argentina-div");
-  if (overview.style.display === "block") {
-      overview.style.display = "none";
-  } else {
-    if (argentina.style.display ==="block"){
-      argentina.style.display = "none";
-    }
-      overview.style.display = "block";
-      headerEl.textContent = "Overview";
-  }
-  closeNav();
-}
-
-function argentinaDisplay() {
-  var headerEl = getEl("main-nav-header");
-  var argentina = getEl("argentina-div");
-  var overview = getEl("overview-div");
-  if (argentina.style.display === "block") {
-      argentina.style.display = "none";
-  } else {
-    if (overview.style.display ==="block"){
-      overview.style.display = "none";
-    }
-      argentina.style.display = "block";
-      headerEl.textContent = "Argentina";
-  }
-  closeNav();
-}
-
 function openStats(evt, construct) {
     // Declared variables
     var i, tabcontent, tablinks;
