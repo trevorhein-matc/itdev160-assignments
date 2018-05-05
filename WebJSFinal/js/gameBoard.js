094fe83805660bbf4d6de8fa885040300154fe5f
Unknown <heint1@gmatc.matc.edu>
2018-05-05 06:55:08 -0500

    Added markers and click handlers to map with sidebar

--- WebJSFinal/js/gameBoard.js
+++ WebJSFinal/js/gameBoard.js
@@ -38,112 +38,40 @@ var totalInvest = 0;
 
 var crops = [
   {
-    name: 'Plantain',
-    type: 'Domestic',
+    name: 'Domestic',
     cost: 1,
     value: 1,
     amount: 0,
     total: 0,
+    yield: 0,
     selector: 'c1'
   },
   {
-    name: 'Bananas',
-    type: 'Domestic',
+    name: 'Livestock',
     cost: 1,
     value: 2,
     amount: 0,
     total: 0,
+    yield: 0,
     selector: 'c2'
   },
   {
-    name: 'Pineapples',
-    type: 'Domestic',
+    name: 'Cash',
     cost: 2,
     value: 2,
     amount: 0,
     total: 0,
+    yield: 0,
     selector: 'c3'
   },
   {
-    name: 'Sugar',
-    type: 'Cash',
+    name: 'Illegal',
     cost: 3,
     value: 3,
     amount: 0,
     total: 0,
+    yield: 0,
     selector: 'c4'
-  },
-  {
-    name: 'Coffee',
-    type: 'Cash',
-    cost: 3,
-    value: 4,
-    amount: 0,
-    total: 0,
-    selector: 'c5'
-  },
-  {
-    name: 'Cotton',
-    type: 'Cash',
-    cost: 4,
-    value: 4,
-    amount: 0,
-    total: 0,
-    selector: 'c6'
-  },
-  {
-    name: 'Tobacco',
-    type: 'Cash',
-    cost: 4,
-    value: 5,
-    amount: 0,
-    total: 0,
-    selector: 'c7'
-  },
-  {
-    name: 'Goats',
-    type: 'Livestock',
-    cost: 2,
-    value: 3,
-    amount: 0,
-    total: 0,
-    selector: 'c8'
-  },
-  {
-    name: 'Cattle',
-    type: 'Livestock',
-    cost: 5,
-    value: 6,
-    amount: 0,
-    total: 0,
-    selector: 'c9'
-  },
-  {
-    name: 'Marijuana',
-    type: 'Illegal',
-    cost: 1,
-    value: 3,
-    amount: 0,
-    total: 0,
-    selector: 'c10'
-  },
-  {
-    name: 'Coca',
-    type: 'Illegal',
-    cost: 4,
-    value: 6,
-    amount: 0,
-    total: 0,
-    selector: 'c11'
-  },
-  {
-    name: 'Opium',
-    type: 'Illegal',
-    cost: 3,
-    value: 6,
-    amount: 0,
-    total: 0,
-    selector: 'c12'
   }
 ];
 
@@ -231,172 +159,13 @@ function sugarSellBtn() {
   }
 }
 
-function coffeePurchaseBtn() {
-  crops[4].amount ++;
-  getEl("c5-amount").innerHTML = crops[4].amount;
-  crops[4].total = crops[4].cost * crops[4].amount;
-  getEl("c5-total").innerHTML = crops[4].total;
-  writeCropTotalCost(crops);
-  return true
-}
-
-function coffeeSellBtn() {
-  if (crops[4].amount > 0) {
-    crops[4].amount --;
-    getEl("c5-amount").innerHTML = crops[4].amount;
-    crops[4].total = crops[4].cost * crops[4].amount;
-    getEl("c5-total").innerHTML = crops[4].total;
-    writeCropTotalCost(crops);
-    return true
-  }
-}
-
-function cottonPurchaseBtn() {
-  crops[5].amount ++;
-  getEl("c6-amount").innerHTML = crops[5].amount;
-  crops[5].total = crops[5].cost * crops[5].amount;
-  getEl("c6-total").innerHTML = crops[5].total;
-  writeCropTotalCost(crops);
-  return true
-}
-
-function cottonSellBtn() {
-  if (crops[5].amount > 0) {
-    crops[5].amount --;
-    getEl("c6-amount").innerHTML = crops[5].amount;
-    crops[5].total = crops[5].cost * crops[5].amount;
-    getEl("c6-total").innerHTML = crops[5].total;
-    writeCropTotalCost(crops);
-    return true
-  }
-}
-
-function tobaccoPurchaseBtn() {
-  crops[6].amount ++;
-  getEl("c7-amount").innerHTML = crops[6].amount;
-  crops[6].total = crops[6].cost * crops[6].amount;
-  getEl("c7-total").innerHTML = crops[6].total;
-  writeCropTotalCost(crops);
-  return true
-}
-
-function tobaccoSellBtn() {
-  if (crops[6].amount > 0) {
-    crops[6].amount --;
-    getEl("c7-amount").innerHTML = crops[6].amount;
-    crops[6].total = crops[6].cost * crops[6].amount;
-    getEl("c7-total").innerHTML = crops[6].total;
-    writeCropTotalCost(crops);
-    return true
-  }
-}
-function goatsPurchaseBtn() {
-  crops[7].amount ++;
-  getEl("c8-amount").innerHTML = crops[7].amount;
-  crops[7].total = crops[7].cost * crops[7].amount;
-  getEl("c8-total").innerHTML = crops[7].total;
-  writeCropTotalCost(crops);
-  return true
-}
-
-function goatsSellBtn() {
-  if (crops[7].amount > 0) {
-    crops[7].amount --;
-    getEl("c8-amount").innerHTML = crops[7].amount;
-    crops[7].total = crops[7].cost * crops[7].amount;
-    getEl("c8-total").innerHTML = crops[7].total;
-    writeCropTotalCost(crops);
-    return true
-  }
-}
-
-function cattlePurchaseBtn() {
-  crops[8].amount ++;
-  getEl("c9-amount").innerHTML = crops[8].amount;
-  crops[8].total = crops[8].cost * crops[8].amount;
-  getEl("c9-total").innerHTML = crops[8].total;
-  writeCropTotalCost(crops);
-  return true
-}
-
-function cattleSellBtn() {
-  if (crops[8].amount > 0) {
-    crops[8].amount --;
-    getEl("c9-amount").innerHTML = crops[8].amount;
-    crops[8].total = crops[8].cost * crops[8].amount;
-    getEl("c9-total").innerHTML = crops[8].total;
-    writeCropTotalCost(crops);
-    return true
-  }
-}
-
-function marijuanaPurchaseBtn() {
-  crops[9].amount ++;
-  getEl("c10-amount").innerHTML = crops[9].amount;
-  crops[9].total = crops[9].cost * crops[9].amount;
-  getEl("c10-total").innerHTML = crops[9].total;
-  writeCropTotalCost(crops);
-  return true
-}
-
-function marijuanaSellBtn() {
-  if (crops[9].amount > 0) {
-    crops[9].amount --;
-    getEl("c10-amount").innerHTML = crops[9].amount;
-    crops[9].total = crops[9].cost * crops[9].amount;
-    getEl("c10-total").innerHTML = crops[9].total;
-    writeCropTotalCost(crops);
-    return true
-  }
-}
-
-function cocaPurchaseBtn() {
-  crops[10].amount ++;
-  getEl("c11-amount").innerHTML = crops[10].amount;
-  crops[10].total = crops[10].cost * crops[10].amount;
-  getEl("c11-total").innerHTML = crops[10].total;
-  writeCropTotalCost(crops);
-  return true
-}
-
-function cocaSellBtn() {
-  if (crops[10].amount > 0) {
-    crops[10].amount --;
-    getEl("c11-amount").innerHTML = crops[10].amount;
-    crops[10].total = crops[10].cost * crops[10].amount;
-    getEl("c11-total").innerHTML = crops[10].total;
-    writeCropTotalCost(crops);
-    return true
-  }
-}
-
-function opiumPurchaseBtn() {
-  crops[11].amount ++;
-  getEl("c12-amount").innerHTML = crops[11].amount;
-  crops[11].total = crops[11].cost * crops[11].amount;
-  getEl("c12-total").innerHTML = crops[11].total;
-  writeCropTotalCost(crops);
-  return true
-}
-
-function opiumSellBtn() {
-  if (crops[11].amount > 0) {
-    crops[11].amount --;
-    getEl("c12-amount").innerHTML = crops[11].amount;
-    crops[11].total = crops[11].cost * crops[11].amount;
-    getEl("c12-total").innerHTML = crops[11].total;
-    writeCropTotalCost(crops);
-    return true
-  }
-}
-
 function Crop(crops) {
   this.name = crops.name;
-  this.type = crops.type;
   this.cost = crops.cost;
   this.value = crops.value;
   this.amount = crops.amount;
   this.total = crops.cost * crops.amount;
+  this.yield = crops.yield;
   this.selector = crops.selector;
 
   this.getFormattedCost = function() {
@@ -414,20 +183,21 @@ function Crop(crops) {
   this.getFormattedTotal = function() {
     return this.total.toLocaleString();
   };
+
+  this.getFormattedYield = function() {
+    return this.yield.toLocaleString();
+  };
 }
 
 var writeCropInfo = function(crop) {
-
   var selector = crop.selector,
     nameEl = getEl(selector + '-name'),
-    typeEl = getEl(selector + '-type'),
     costEl = getEl(selector + '-cost'),
     valueEl = getEl(selector + '-value'),
     amountEl = getEl(selector + '-amount'),
     totalEl = getEl(selector + '-total')
 
     nameEl.textContent = crop.name;
-    typeEl.textContent = crop.type;
     costEl.textContent = crop.getFormattedCost();
     valueEl.textContent = crop.getFormattedValue();
     amountEl.textContent = crop.getFormattedAmount();
@@ -446,7 +216,7 @@ var writeCropTotalCost = function(crops) {
   var laborCostPerAcre = 0.25;
   var laborCostTotal = 0;
   var savings = 0;
-  var bank = 25;
+  var turnBank = bank;
   var totalInvest = 0;
   for (var t = 0; t < crops.length; t++){
     cropCost = cropCost + crops[t].total;
@@ -455,7 +225,7 @@ var writeCropTotalCost = function(crops) {
   }
 
   totalInvest = (cropCost + laborCostTotal);
-  savings = bank - totalInvest;
+  savings = turnBank - totalInvest;
 
   cropCostEl = getEl('cropTotalCost'),
   landUseEl = getEl('landUse'),
@@ -470,9 +240,31 @@ var writeCropTotalCost = function(crops) {
   investEl.innerHTML = totalInvest;
 }
 
-/*
-var writeBankTotals = function(crops) {
-  totalInvest = (cropCost + landUse + laborCostTotal);
-  bank
+var yieldCalc = function() {
+  var domCropAmount = crops[0].amount;
+  var livestockAmount = crops[1].amount;
+  var cashCropAmount = crops[2].amount;
+  var illegalCropAmount = crops[3].amount;
+
+
 }
-*/
+
+
+var writeYieldInfo = function(cropYield) {
+  var selector = cropYield.selector,
+    nameEl = getEl(selector + '-yieldName'),
+    amountEl = getEl(selector + '-yieldAmount'),
+    totalEl = getEl(selector + '-yieldTotal')
+
+    nameEl.textContent = cropYield.name;
+    amountEl.textContent = cropYield.getFormattedAmount();
+    totalEl.textContent = cropYield.getFormattedYield();
+}
+
+// Writes the crops table to the DOM
+var yieldButton = function() {
+  for (var i = 0; i < crops.length; i++) {
+    var cropYield = new Crop(crops[i]);
+    writeCropInfo(cropYield);
+  }
+}
--- WebJSFinal/js/map.js
+++ WebJSFinal/js/map.js
@@ -1,11 +1,73 @@
 var mymap = L.map('mapid').setView([-4.010695, -71.497656], 3);
 
 L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
-    attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
     maxZoom: 18,
     id: 'mapbox.pirates',
     accessToken: 'pk.eyJ1IjoidHJlaGVpbmd1eSIsImEiOiJjamZjcXNtZmQyM3RlM3VtemloN3hnZWdlIn0.tNjmCotRY5nY7dlal1X-jA'
 }).addTo(mymap);
+
+var blueIcon = new L.Icon({
+	iconUrl: '../images/marker-icon-blue.png',
+	shadowUrl: '../images/marker-shadow.png',
+	iconSize: [25, 41],
+	iconAnchor: [12, 41],
+	popupAnchor: [1, -34],
+	shadowSize: [41, 41]
+});
+
+var redIcon = new L.Icon({
+	iconUrl: '../images/marker-icon-red.png',
+	shadowUrl: '../images/marker-shadow.png',
+	iconSize: [25, 41],
+	iconAnchor: [12, 41],
+	popupAnchor: [1, -34],
+	shadowSize: [41, 41]
+});
+
+var greenIcon = new L.Icon({
+	iconUrl: '../images/marker-icon-green.png',
+	shadowUrl: '../images/marker-shadow.png',
+	iconSize: [25, 41],
+	iconAnchor: [12, 41],
+	popupAnchor: [1, -34],
+	shadowSize: [41, 41]
+});
+
+var orangeIcon = new L.Icon({
+	iconUrl: '../images/marker-icon-orange.png',
+	shadowUrl: '../images/marker-shadow.png',
+	iconSize: [25, 41],
+	iconAnchor: [12, 41],
+	popupAnchor: [1, -34],
+	shadowSize: [41, 41]
+});
+
+var yellowIcon = new L.Icon({
+	iconUrl: '../images/marker-icon-yellow.png',
+	shadowUrl: '../images/marker-shadow.png',
+	iconSize: [25, 41],
+	iconAnchor: [12, 41],
+	popupAnchor: [1, -34],
+	shadowSize: [41, 41]
+});
+
+var violetIcon = new L.Icon({
+	iconUrl: '../images/marker-icon-violet.png',
+	shadowUrl: '../images/marker-shadow.png',
+	iconSize: [25, 41],
+	iconAnchor: [12, 41],
+	popupAnchor: [1, -34],
+	shadowSize: [41, 41]
+});
+
+var blackIcon = new L.Icon({
+	iconUrl: '../images/marker-icon-black.png',
+	shadowUrl: '../images/marker-shadow.png',
+	iconSize: [25, 41],
+	iconAnchor: [12, 41],
+	popupAnchor: [1, -34],
+	shadowSize: [41, 41]
+});
 
 /*
 var popup = L.popup()
@@ -20,6 +82,7 @@ function onMapClick(e) {
 mymap.on('click', onMapClick);
 
 
+
 var popup = L.popup();
 
 function onMapClick(e) {
@@ -32,34 +95,124 @@ function onMapClick(e) {
 mymap.on('click', onMapClick);
 */
 
-var argentina = L.circle([-34.603684, -58.381559], {color: 'green', fillColor: 'green', fillOpacity: 0.5, radius: 100000}).addTo(mymap);
-var bolivia = L.circle([-16.489689, -68.119294], {color: 'blue', fillColor: 'blue', fillOpacity: 0.5, radius: 100000}).addTo(mymap);
-var brazil = L.circle([-23.550520, -46.633309], {color: 'green', fillColor: 'green', fillOpacity: 0.5, radius: 100000}).addTo(mymap);
-var chile = L.circle([-33.448890, -70.669265], {color: 'red', fillColor: 'red', fillOpacity: 0.5, radius: 100000}).addTo(mymap);
-var colombia = L.circle([4.710989, -74.072092], {color: 'red', fillColor: 'red', fillOpacity: 0.5, radius: 100000}).addTo(mymap);
-var costaRica = L.circle([9.928069, -84.090725], {color: 'green', fillColor: 'green', fillOpacity: 0.5, radius: 100000}).addTo(mymap);
-var cuba = L.circle([23.113592, -82.366596], {color: 'red', fillColor: 'red', fillOpacity: 0.5, radius: 100000}).addTo(mymap);
-var dominicanRepublic = L.circle([18.765850, -69.040668], {color: 'green', fillColor: 'green', fillOpacity: 0.5, radius: 100000}).addTo(mymap);
-var ecuador = L.circle([-0.180653, -78.467838], {color: 'green', fillColor: 'green', fillOpacity: 0.5, radius: 100000}).addTo(mymap);
-var elSalvador = L.circle([13.692940, -89.218191], {color: 'red', fillColor: 'red', fillOpacity: 0.5, radius: 100000}).addTo(mymap);
-var guatemala = L.circle([14.634915, -90.506882], {color: 'red', fillColor: 'red', fillOpacity: 0.5, radius: 100000}).addTo(mymap);
-var haiti = L.circle([18.594395, -72.307433], {color: 'green', fillColor: 'green', fillOpacity: 0.5, radius: 100000}).addTo(mymap);
-var honduras = L.circle([14.072275, -87.192136], {color: 'red', fillColor: 'red', fillOpacity: 0.5, radius: 100000}).addTo(mymap);
-var jamaica = L.circle([18.476223, -77.89389], {color: 'green', fillColor: 'green', fillOpacity: 0.5, radius: 100000}).addTo(mymap);
-var mexico = L.circle([19.432608, -99.133208], {color: 'red', fillColor: 'red', fillOpacity: 0.5, radius: 100000}).addTo(mymap);
-var nicaragua = L.circle([12.114993, -86.236174], {color: 'red', fillColor: 'red', fillOpacity: 0.5, radius: 100000}).addTo(mymap);
-var panama = L.circle([8.537981, -80.782127], {color: 'green', fillColor: 'green', fillOpacity: 0.5, radius: 100000}).addTo(mymap);
-var paraguay = L.circle([-25.263740, -57.575926], {color: 'blue', fillColor: 'blue', fillOpacity: 0.5, radius: 100000}).addTo(mymap);
-var peru = L.circle([-12.046373, -77.042754], {color: 'red', fillColor: 'red', fillOpacity: 0.5, radius: 100000}).addTo(mymap);
-var puertoRico = L.circle([18.465539, -66.105735], {color: 'red', fillColor: 'red', fillOpacity: 0.5, radius: 100000}).addTo(mymap);
-var uruguay = L.circle([-34.901113, -56.164531], {color: 'red', fillColor: 'red', fillOpacity: 0.5, radius: 100000}).addTo(mymap);
-var venezuela = L.circle([10.480594, -66.903606], {color: 'blue', fillColor: 'blue', fillOpacity: 0.5, radius: 100000}).addTo(mymap);
+
+
+var argentina = L.marker([-34.603684, -58.381559], {
+	icon: blackIcon
+}).addTo(mymap).on('click', function () {
+  sidebar.toggle();
+});
+var bolivia = L.marker([-16.489689, -68.119294], {
+	icon: blackIcon
+}).addTo(mymap).on('click', function () {
+  sidebar.toggle();
+});
+var brazil = L.marker([-23.550520, -46.633309], {
+	icon: greenIcon
+}).addTo(mymap).on('click', function () {
+  sidebar.toggle();
+});
+var chile = L.marker([-33.448890, -70.669265], {
+	icon: redIcon
+}).addTo(mymap).on('click', function () {
+  sidebar.toggle();
+});
+var colombia = L.marker([4.710989, -74.072092], {
+	icon: redIcon
+}).addTo(mymap).on('click', function () {
+  sidebar.toggle();
+});
+var costaRica = L.marker([9.928069, -84.090725], {
+	icon: greenIcon
+}).addTo(mymap).on('click', function () {
+  sidebar.toggle();
+});
+var cuba = L.marker([23.113592, -82.366596], {
+	icon: redIcon
+}).addTo(mymap).on('click', function () {
+  sidebar.toggle();
+});
+var dominicanRepublic = L.marker([18.765850, -69.040668], {
+	icon: greenIcon
+}).addTo(mymap).on('click', function () {
+  sidebar.toggle();
+});
+var ecuador = L.marker([-0.180653, -78.467838], {
+	icon: greenIcon
+}).addTo(mymap).on('click', function () {
+  sidebar.toggle();
+});
+var elSalvador = L.marker([13.692940, -89.218191], {
+	icon: redIcon
+}).addTo(mymap).on('click', function () {
+  sidebar.toggle();
+});
+var guatemala = L.marker([14.634915, -90.506882], {
+	icon: redIcon
+}).addTo(mymap).on('click', function () {
+  sidebar.toggle();
+});
+var haiti = L.marker([18.594395, -72.307433], {
+	icon: greenIcon
+}).addTo(mymap).on('click', function () {
+  sidebar.toggle();
+});
+var honduras = L.marker([14.072275, -87.192136], {
+	icon: redIcon
+}).addTo(mymap).on('click', function () {
+  sidebar.toggle();
+});
+var jamaica = L.marker([18.476223, -77.89389], {
+	icon: greenIcon
+}).addTo(mymap).on('click', function () {
+  sidebar.toggle();
+});
+var mexico = L.marker([19.432608, -99.133208], {
+	icon: redIcon
+}).addTo(mymap).on('click', function () {
+  sidebar.toggle();
+});
+var nicaragua = L.marker([12.114993, -86.236174], {
+	icon: redIcon
+}).addTo(mymap).on('click', function () {
+  sidebar.toggle();
+});
+var panama = L.marker([8.537981, -80.782127], {
+	icon: greenIcon
+}).addTo(mymap).on('click', function () {
+  sidebar.toggle();
+});
+var paraguay = L.marker([-25.263740, -57.575926], {
+	icon: blackIcon
+}).addTo(mymap).on('click', function () {
+  sidebar.toggle();
+});
+var peru = L.marker([-12.046373, -77.042754], {
+	icon: blackIcon
+}).addTo(mymap).on('click', function () {
+  sidebar.toggle();
+});
+var puertoRico = L.marker([18.465539, -66.105735], {
+	icon: blackIcon
+}).addTo(mymap).on('click', function () {
+  sidebar.toggle();
+});
+var uruguay = L.marker([-34.901113, -56.164531], {
+	icon: blackIcon
+}).addTo(mymap).on('click', function () {
+  sidebar.toggle();
+});
+var venezuela = L.marker([10.480594, -66.903606], {
+	icon: blackIcon
+}).addTo(mymap).on('click', function () {
+  sidebar.toggle();
+});
 
 var allCountries = L.layerGroup([argentina, bolivia, brazil, chile, colombia, costaRica, cuba, dominicanRepublic, ecuador, elSalvador, guatemala, haiti, honduras, jamaica, mexico, nicaragua, panama, paraguay, peru, puertoRico, uruguay, venezuela]);
 
-var economic = L.layerGroup([argentina, brazil, costaRica, dominicanRepublic, ecuador, haiti, jamaica, panama]);
+var economic = L.layerGroup([brazil, costaRica, dominicanRepublic, ecuador, haiti, jamaica, panama]);
 
-var political = L.layerGroup([bolivia, guatemala, jamaica, mexico, panama, paraguay, peru, puertoRico, uruguay, venezuela]);
+var political = L.layerGroup([argentina, bolivia, paraguay, peru, puertoRico, uruguay, venezuela]);
 
 var military = L.layerGroup([colombia, chile, cuba, elSalvador, guatemala, honduras, mexico, nicaragua]);
 
@@ -72,4 +225,219 @@ var overlayMaps = {
 
 L.control.layers(overlayMaps, null).addTo(mymap)
 
-//cuba.bindPopup("<b>Hello world!</b><br>I am a popup.");
+cuba.bindPopup("<b>Hello world!</b><br>I am a popup.");
+
+// Adding sidenav with functions
+
+L.Control.Sidebar = L.Control.extend({
+
+    includes: L.Mixin.Events,
+
+    options: {
+        closeButton: true,
+        position: 'left',
+        autoPan: true,
+    },
+
+    initialize: function (placeholder, options) {
+        L.setOptions(this, options);
+
+        // Find content container
+        var content = this._contentContainer = L.DomUtil.get(placeholder);
+
+        // Remove the content container from its original parent
+        content.parentNode.removeChild(content);
+
+        var l = 'leaflet-';
+
+        // Create sidebar container
+        var container = this._container =
+            L.DomUtil.create('div', l + 'sidebar ' + this.options.position);
+
+        // Style and attach content container
+        L.DomUtil.addClass(content, l + 'control');
+        container.appendChild(content);
+
+        // Create close button and attach it if configured
+        if (this.options.closeButton) {
+            var close = this._closeButton =
+                L.DomUtil.create('a', 'close', container);
+            close.innerHTML = '&times;';
+        }
+    },
+
+    addTo: function (mymap) {
+        var container = this._container;
+        var content = this._contentContainer;
+
+        // Attach event to close button
+        if (this.options.closeButton) {
+            var close = this._closeButton;
+
+            L.DomEvent.on(close, 'click', this.hide, this);
+        }
+
+        L.DomEvent
+            .on(container, 'transitionend',
+                this._handleTransitionEvent, this)
+            .on(container, 'webkitTransitionEnd',
+                this._handleTransitionEvent, this);
+
+        // Attach sidebar container to controls container
+        var controlContainer = mymap._controlContainer;
+        controlContainer.insertBefore(container, controlContainer.firstChild);
+
+        this._map = mymap;
+
+        // Make sure we don't drag the map when we interact with the content
+        var stop = L.DomEvent.stopPropagation;
+        var fakeStop = L.DomEvent._fakeStop || stop;
+        L.DomEvent
+            .on(content, 'contextmenu', stop)
+            .on(content, 'click', fakeStop)
+            .on(content, 'mousedown', stop)
+            .on(content, 'touchstart', stop)
+            .on(content, 'dblclick', fakeStop)
+            .on(content, 'mousewheel', stop)
+            .on(content, 'MozMousePixelScroll', stop);
+
+        return this;
+    },
+
+    removeFrom: function (mymap) {
+        //if the control is visible, hide it before removing it.
+        this.hide();
+
+        var container = this._container;
+        var content = this._contentContainer;
+
+        // Remove sidebar container from controls container
+        var controlContainer = mymap._controlContainer;
+        controlContainer.removeChild(container);
+
+        //disassociate the map object
+        this._map = null;
+
+        // Unregister events to prevent memory leak
+        var stop = L.DomEvent.stopPropagation;
+        var fakeStop = L.DomEvent._fakeStop || stop;
+        L.DomEvent
+            .off(content, 'contextmenu', stop)
+            .off(content, 'click', fakeStop)
+            .off(content, 'mousedown', stop)
+            .off(content, 'touchstart', stop)
+            .off(content, 'dblclick', fakeStop)
+            .off(content, 'mousewheel', stop)
+            .off(content, 'MozMousePixelScroll', stop);
+
+        L.DomEvent
+            .off(container, 'transitionend',
+                this._handleTransitionEvent, this)
+            .off(container, 'webkitTransitionEnd',
+                this._handleTransitionEvent, this);
+
+        if (this._closeButton && this._close) {
+            var close = this._closeButton;
+
+            L.DomEvent.off(close, 'click', this.hide, this);
+        }
+
+        return this;
+    },
+
+    isVisible: function () {
+        return L.DomUtil.hasClass(this._container, 'visible');
+    },
+
+    show: function () {
+        if (!this.isVisible()) {
+            L.DomUtil.addClass(this._container, 'visible');
+            if (this.options.autoPan) {
+                this._map.panBy([-this.getOffset() / 2, 0], {
+                    duration: 0.5
+                });
+            }
+            this.fire('show');
+        }
+    },
+
+    hide: function (e) {
+        if (this.isVisible()) {
+            L.DomUtil.removeClass(this._container, 'visible');
+            if (this.options.autoPan) {
+                this._map.panBy([this.getOffset() / 2, 0], {
+                    duration: 0.5
+                });
+            }
+            this.fire('hide');
+        }
+        if(e) {
+            L.DomEvent.stopPropagation(e);
+        }
+    },
+
+    toggle: function () {
+        if (this.isVisible()) {
+            this.hide();
+        } else {
+            this.show();
+        }
+    },
+
+    getContainer: function () {
+        return this._contentContainer;
+    },
+
+    getCloseButton: function () {
+        return this._closeButton;
+    },
+
+    setContent: function (content) {
+        var container = this.getContainer();
+
+        if (typeof content === 'string') {
+            container.innerHTML = content;
+        } else {
+            // clean current content
+            while (container.firstChild) {
+                container.removeChild(container.firstChild);
+            }
+
+            container.appendChild(content);
+        }
+
+        return this;
+    },
+
+    getOffset: function () {
+        if (this.options.position === 'right') {
+            return -this._container.offsetWidth;
+        } else {
+            return this._container.offsetWidth;
+        }
+    },
+
+    _handleTransitionEvent: function (e) {
+        if (e.propertyName == 'left' || e.propertyName == 'right')
+            this.fire(this.isVisible() ? 'shown' : 'hidden');
+    }
+});
+
+L.control.sidebar = function (placeholder, options) {
+    return new L.Control.Sidebar(placeholder, options);
+};
+
+var sidebar = L.control.sidebar('sidebar', {
+    closeButton: true,
+    position: 'left'
+});
+
+mymap.addControl(sidebar);
+
+setTimeout(function () {
+    sidebar.show();
+}, 500);
+
+mymap.on('click', function () {
+    sidebar.hide();
+})
